28f04a5a27397f3a0c66f69ea8d67f58d8e580e9
MAHATHI <mahathitalluru@gmail.com>
2025-12-22 16:01:25 -0600

    routes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
-app.use("/test",(req,res) => {
-    res.send("Hello from the server");
+// app.use("/test",(req,res) => {
+//     res.send("Hello from the server");
+// });
+// app.use("/namaste",(req,res) => {
+//     res.send("Hello from the servers");
+// });
+app.get("/user/:userId/:userName/:userRole",(req,res) => {
+    console.log(req.params);
+    res.send({FirstName:"Mahathi", lastName:"Talluru"});
 });
-app.use("/namaste",(req,res) => {
-    res.send("Hello from the servers");
+app.get("/user",(req,res)=>{
+    console.log(req.query);
+    res.send({FirstName:"Mahathi",LastName:"Talluru"})
 });
-app.use("/",(req,res) => {
-     res.send("Welocome to dev tinder");
-})
-app.get("/test");
+
+// app.get("/test");
 
 app.listen(3000,()=>{
   console.log("server is successfully listening on 3000")
